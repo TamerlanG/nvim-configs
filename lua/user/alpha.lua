16bcc5518f3bbc781707be0ae2476e3a278c918a
Tamerlan Gudabayev <tamerlanalbehairy@gmail.com>
2023-07-18 13:31:09 +0200

    update config

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -19,16 +19,17 @@ dashboard.section.buttons.val = {
 	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
 	dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
 	dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
+  dashboard.button("z", "  Configure Zsh", ":e ~/.zshrc <CR>"),
 	dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
 }
 
 local function footer()
 -- NOTE: requires the fortune-mod package to work
-	-- local handle = io.popen("fortune")
-	-- local fortune = handle:read("*a")
-	-- handle:close()
-	-- return fortune
-	return "tamerlan.dev"
+	local handle = io.popen("fortune")
+	local fortune = handle:read("*a")
+	handle:close()
+	return fortune
+	-- return "tamerlan.dev"
 end
 
 dashboard.section.footer.val = footer()
