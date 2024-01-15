3658139f6033be23bd34f129bc17956f9dd7dae1
Tamerlan Gudabayev <tamerlanalbehairy@gmail.com>
2024-01-15 09:13:34 +0100

    feat: add noice

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -130,6 +130,25 @@ local plugins = {
 		branch = "harpoon2",
 		dependencies = { "nvim-lua/plenary.nvim" },
 	},
+
+	"MunifTanjim/nui.nvim",
+
+	"rcarriga/nvim-notify",
+
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {},
+
+		dependencies = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+			"rcarriga/nvim-notify",
+		},
+	},
 }
 
 require("lazy").setup(plugins, {})
