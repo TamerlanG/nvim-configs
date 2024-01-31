4e8819e2c020dfd49917f088b688b0e67ede0ea3
Tamerlan Gudabayev <tamerlanalbehairy@gmail.com>
2024-01-31 21:46:36 +0100

    feat: keymap ctrl d and u with zz

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -64,3 +64,7 @@ keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 -- keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 -- keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 -- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+
+-- Vertical Scrolling --
+keymap("n", "<C-d>", "<C-d>zz", opts)
+keymap("n", "<C-u>", "<C-u>zz", opts)
